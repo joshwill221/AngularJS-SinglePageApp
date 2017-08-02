58528c0f183aa56518b0a6fc2574e9722c3ebb05
joshwill221 <joshwill221@gmail.com>
2017-08-02 16:06:08 +0800

    Fixed bug / typos. Basic controller functionality is working.

--- script.js
+++ script.js
@@ -9,19 +9,19 @@ scotchApp.config(function($routeProvider) {
     $routeProvider
     
         // route for the home page
-        .when('/' {
+        .when('/', {
             templateUrl : 'pages/home.html',
             controller  : 'mainController'
         })
     
         // route for the about page
-        .when('/about' {
+        .when('/about', {
             templateUrl : 'pages/about.html',
             controller  : 'aboutController'
         })
     
         // route for the contact page
-        .when('/contact' {
+        .when('/contact', {
             templateUrl : 'pages/contact.html',
             controller  : 'contactController'
         });
@@ -33,10 +33,10 @@ scotchApp.controller('mainController', function($scope) {
     $scope.message = 'Everyone come and see how good I look!';
 });
 
-scotchApp.controller('aboutController', fucntion($scope) {
+scotchApp.controller('aboutController', function($scope) {
     $scope.message = 'Look! I am an about page.';
 });
 
-scotchApp.controller('contactController', fucntion($scope) {
+scotchApp.controller('contactController', function($scope) {
     $scope.message = 'Contact us! JK. This is just a demo.';
 });
